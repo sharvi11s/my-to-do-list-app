46010be01e23dd068679a03d6ee3140c0411e3e9
sharvi11s <sharvis4251csefy2@gmail.com>
2025-08-23 20:36:59 +0530

    edited title in app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <div className="bg-white shadow-lg rounded-2xl p-8 w-full max-w-sm sm:max-w-md lg:max-w-lg text-center transition-all duration-300">
         <img src={logo} alt="logo" className="w-24 h-24 mx-auto mb-4" />
         <h1 className="text-3xl font-bold text-pink-600 mb-6">
-          🌸 My Cute To-Do List
+          🌸 My To-Do List
         </h1>
         <TodoApp />
       </div>
